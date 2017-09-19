ad9ae760adb403d38c1fd60317262a9c487d7b51
¨gaurhari¨ <gaurharidass@gmail.com>
2017-09-19 04:56:56 +0100

    updating similarity service

--- src/main/java/uk/ac/ebi/ddi/pipeline/indexer/tasklet/enrichment/SimilarsEnrichmentTasklet.java
+++ src/main/java/uk/ac/ebi/ddi/pipeline/indexer/tasklet/enrichment/SimilarsEnrichmentTasklet.java
@@ -6,6 +6,12 @@ import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.util.Assert;
 import uk.ac.ebi.ddi.annotation.service.dataset.DDIDatasetAnnotationService;
 import uk.ac.ebi.ddi.pipeline.indexer.tasklet.AbstractTasklet;
+import uk.ac.ebi.ddi.service.db.model.publication.PublicationDataset;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 /**
  * This code is licensed under the Apache License, Version 2.0 (the
@@ -26,17 +32,17 @@ public class SimilarsEnrichmentTasklet extends AbstractTasklet{
 
     @Override
     public RepeatStatus execute(StepContribution stepContribution, ChunkContext chunkContext) throws Exception {
-//        List<PublicationDataset> datasetList = datasetAnnotationService.getPublicationDatasets();
-//        datasetList = datasetList.parallelStream().filter(x -> x.getOmicsType() != null && !x.getOmicsType().isEmpty()).collect(Collectors.toList());
-//        Map<String, Set<PublicationDataset>> publicationMap = datasetList.parallelStream().collect(Collectors.groupingBy(PublicationDataset::getPubmedId, Collectors.toSet()));
-//        publicationMap.entrySet().parallelStream().forEach( publication -> {
-//            publication.getValue().parallelStream().forEach( x -> {
-//                List<PublicationDataset> similars = publication.getValue().stream().filter(dat -> dat.getDatasetID() != x.getDatasetID()).collect(Collectors.toList());
-//                Map<String, Set<String>> similarMap = similars.parallelStream().collect(Collectors.groupingBy(PublicationDataset::getDatabase, Collectors.mapping(PublicationDataset::getDatasetID, Collectors.toSet())));
-//                datasetAnnotationService.updateDatasetSimilars(x.getDatasetID(), x.getDatabase(), similarMap);
-//                });
-//            }
-//        );
+        List<PublicationDataset> datasetList = datasetAnnotationService.getPublicationDatasets();
+        datasetList = datasetList.parallelStream().filter(x -> x.getOmicsType() != null && !x.getOmicsType().isEmpty()).collect(Collectors.toList());
+        Map<String, Set<PublicationDataset>> publicationMap = datasetList.parallelStream().collect(Collectors.groupingBy(PublicationDataset::getPubmedId, Collectors.toSet()));
+        publicationMap.entrySet().parallelStream().forEach( publication -> {
+           publication.getValue().parallelStream().forEach( x -> {
+                List<PublicationDataset> similars = publication.getValue().stream().filter(dat -> dat.getDatasetID() != x.getDatasetID()).collect(Collectors.toList());
+               Map<String, Set<String>> similarMap = similars.parallelStream().collect(Collectors.groupingBy(PublicationDataset::getDatabase, Collectors.mapping(PublicationDataset::getDatasetID, Collectors.toSet())));
+                //datasetAnnotationService.updateDatasetSimilars(x.getDatasetID(), x.getDatabase(), similarMap);
+                });
+            }
+        );
           return RepeatStatus.FINISHED;
     }
 

4f37b67977971944deff33310351b029842f17fc
¨gaurhari¨ <gaurharidass@gmail.com>
2018-04-09 19:30:41 +0100

    private dataset tasklet added

---
+++ src/test/java/uk/ac/ebi/ddi/pipeline/indexer/tasklet/validation/PrivateDatasetTaskletTest.java
@@ -0,0 +1,64 @@
+package uk.ac.ebi.ddi.pipeline.indexer.tasklet.validation;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.batch.core.*;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.test.JobLauncherTestUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"classpath:jobs/privatedatasets.xml"})
+public class PrivateDatasetTaskletTest {
+
+    public static final String INDEXER_PARAMETER = "inderxer.param";
+    public static final String TEST_MODE = "test.mode";
+
+
+    @Autowired
+    private JobLauncher jobLauncher;
+
+
+    @Autowired
+    @Qualifier("ddiPrivateDatasetJob")
+    private Job job;
+
+
+    private JobParameters jobParameters;
+
+    private JobLauncherTestUtils jobLauncherTestUtils;
+
+    @Autowired
+    private JobRepository jobRepository;
+
+    @Before
+    public void setUp() throws Exception {
+        initJobParameters();
+    }
+
+
+    private void initJobParameters() {
+        this.jobParameters =  new JobParametersBuilder().addString(INDEXER_PARAMETER,INDEXER_PARAMETER)
+                .addString(TEST_MODE, "true")
+                .toJobParameters();
+        this.jobLauncherTestUtils = new JobLauncherTestUtils();
+        this.jobLauncherTestUtils.setJobLauncher(jobLauncher);
+        this.jobLauncherTestUtils.setJobRepository(jobRepository);
+        this.jobLauncherTestUtils.setJob(job);
+    }
+
+    @Test
+    public void testLaunchJobWithJobLauncher() throws Exception {
+
+        JobExecution jobExecution = jobLauncher.run(job, jobParameters);
+        assertEquals(BatchStatus.COMPLETED, jobExecution.getStatus());
+    }
+
+}

537d19a6c2e663fea5c55903d6a40a78b72cb162
Gaurhari <gaurharidass@gmail.com>
2018-09-12 16:36:54 +0100

    updated script for reanalysis datasets

--- scripts/correspondingreanalysis.js
+++ scripts/correspondingreanalysis.js
@@ -9,7 +9,24 @@ db.datasets.similars.find({"accession":"PXD000561"}).forEach(function(input_data
                     var dataset = db.datasets.dataset.find({"_id":id});
                     var targetAcccession = dataset.accession;
                     var targetDatabase = dataset.database;
-                    
+                    var sim_data = {
+                        accession :targetAccession,
+                        database : targetDatabase,
+                        "_class" : "uk.ac.ebi.ddi.service.db.model.dataset.DatasetSimilars",
+                        similars : []
+                    }
+                    var similar_datasets = {
+                        relationType : "Reanalysis of",
+                        similarDataset : { 
+                            "$ref" : "datasets.dataset",
+                            "$id" : db.datasets.dataset.findOne({"accession":input_data.accession,"database":input_data.database})._id
+                            } 
+                    };
+                    print(similar_datasets);
+                    sim_data.similars.push(similar_datasets);
+                    print(sim_data);
+                    db.datasets.similars.update({"accession":sim_data.accession,"database":sim_data.database,"_class":sim_data._class},
+                    {$addToSet:{"similars":similar_datasets}},true,true);
 
             });
     });

2835ee11173236fc4ab0fc758ca86fd52997f816
Tu Manh Vu <glmanhtu@gmail.com>
2019-01-16 17:02:55 +0000

    Fixed 429 too many request when running arrayexpress importer pipeline Clean code

--- src/main/java/uk/ac/ebi/ddi/pipeline/indexer/tasklet/annotation/ArrayExpressAnnotationTasklet.java
+++ src/main/java/uk/ac/ebi/ddi/pipeline/indexer/tasklet/annotation/ArrayExpressAnnotationTasklet.java
@@ -12,6 +12,7 @@ import uk.ac.ebi.ddi.service.db.model.dataset.Dataset;
 import uk.ac.ebi.ddi.service.db.utils.DatasetCategory;
 
 import java.util.List;
+import java.util.concurrent.ForkJoinPool;
 
 /**
  * @author Yasset Perez-Riverol (ypriverol@gmail.com)
@@ -23,27 +24,32 @@ public class ArrayExpressAnnotationTasklet extends AnnotationXMLTasklet {
 
     private NCBITaxonomyService taxonomyService = NCBITaxonomyService.getInstance();
 
+    // eutils/esearch currently supported maximum 3 connections per time
+    private static final int PARALLEL = Math.min(3, Runtime.getRuntime().availableProcessors());
+
     @Override
     public RepeatStatus execute(StepContribution stepContribution, ChunkContext chunkContext) throws Exception {
 
         if (databaseName != null) {
             List<Dataset> datasets = datasetAnnotationService.getAllDatasetsByDatabase(databaseName);
-            datasets.parallelStream().forEach(dataset -> {
-                try {
-                    if (dataset.getCurrentStatus().equalsIgnoreCase(DatasetCategory.INSERTED.getType())) {
-                        Dataset exitingDataset = datasetAnnotationService.getDataset(
-                                dataset.getAccession(), dataset.getDatabase());
-                        exitingDataset = DatasetAnnotationFieldsUtils.addpublicationDate(exitingDataset);
-                        exitingDataset = DatasetAnnotationEnrichmentService.updatePubMedIds(
-                                publicationService, exitingDataset);
-                        exitingDataset = taxonomyService.annotateSpecies(exitingDataset);
-                        datasetAnnotationService.annotateDataset(exitingDataset);
-                    }
-                } catch (Exception e) {
-                    LOGGER.error("Exception ocurred when processing dataset {}, ", dataset.getAccession(), e);
-                }
-            });
+            ForkJoinPool customThreadPool = new ForkJoinPool(PARALLEL);
+            customThreadPool.submit(() -> datasets.parallelStream().forEach(this::process)).get();
         }
         return RepeatStatus.FINISHED;
+    }
+
+    private void process(Dataset dataset) {
+        try {
+            if (dataset.getCurrentStatus().equalsIgnoreCase(DatasetCategory.INSERTED.getType())) {
+                Dataset exitingDataset = datasetAnnotationService.getDataset(
+                        dataset.getAccession(), dataset.getDatabase());
+                exitingDataset = DatasetAnnotationFieldsUtils.addpublicationDate(exitingDataset);
+                DatasetAnnotationEnrichmentService.updatePubMedIds(publicationService, exitingDataset);
+                exitingDataset = taxonomyService.annotateSpecies(exitingDataset);
+                datasetAnnotationService.annotateDataset(exitingDataset);
+            }
+        } catch (Exception e) {
+            LOGGER.error("Exception ocurred when processing dataset {}, ", dataset.getAccession(), e);
+        }
     }
 }

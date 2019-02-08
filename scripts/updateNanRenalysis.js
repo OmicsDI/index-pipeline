8b20293aa0cab9c61e180089ecbc1f1b99adcfae
Gaurhari <gaurharidass@gmail.com>
2019-02-08 10:40:31 +0000

    updated script for removing nan reanalysis

--- scripts/updateNanRenalysis.js
+++ scripts/updateNanRenalysis.js
@@ -1,1 +1,1 @@
-db.datasets.dataset.update({"additional.reanalysis_count":"NaN"},{"$set":{"additional.reanalysis_count":["0.0"]}},false,true)
+db.datasets.dataset.update({"additional.reanalysis_count":"NaN"},{"$set":{"additional.reanalysis_count":["0"]}},false,true)

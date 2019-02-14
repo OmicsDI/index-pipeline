399c07e407c99291f5415f2618ae122e2b82e81a
Gaurhari <gaurharidass@gmail.com>
2019-02-14 16:58:50 +0000

    updated from double to

---
+++ scripts/biomodelsmaxreanalysed.js
@@ -0,0 +1,2 @@
+db.datasets.dataset.update({"accession":"MODEL1402200003"},{"$set":{"additional.reanalysis_count":["6733"],"scores.reanalysisCount":6733,
+    "additional.reanalysis_count_scaled":["1"]}})

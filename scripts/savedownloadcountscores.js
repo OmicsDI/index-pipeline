e4f32c311e54ab4b8c21ef9af4c3c06a3df26027
Gaurhari <gaurharidass@gmail.com>
2019-02-14 16:45:36 +0000

    update download count scores to scores object to calculate min and max values

---
+++ scripts/savedownloadcountscores.js
@@ -0,0 +1,6 @@
+db.datasets.dataset.find({"additional.download_count":{"$exists":true}}).forEach(function(input_obj)
+{
+    print(parseInt(input_obj.additional.download_count[0]))
+    db.datasets.dataset.update({"accession":input_obj.accession,"database":input_obj.database},
+    {"$set":{"scores.downloadCount":parseInt(input_obj.additional.download_count[0])}})
+})
